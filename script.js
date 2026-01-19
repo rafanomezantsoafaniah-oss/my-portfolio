b3a59e65c93d26d26dd4c79eea86502ed9c37186
Martin <faniryjean3@gmail.com>
2026-01-19 14:42:55 +0100

    modification

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ toggle.addEventListener('click', () => {
     <i class="fas fa-sun"></i>`
   }
 });
+
+const images = document.querySelector(".projet"); 
+const total = images.children.length;
+    let index = 0;
+    const width = 600;
+    function showImage(i) {
+    images.style.transition = "transform 0.5s ease-in-out";
+    images.style.transform = `translateX(-${i * width}px)`;
+    index = i;    
+}
+    function nextImage() {
+        showImage(index + 1);
+    if(index == total-1){
+        setTimeout(() =>{
+            images.style.transition = "none";
+            images.style.transform = "translateX(0px)";
+            index = 0
+        },500)
+    }
+    }
+    function prevImage() {
+        if(index == 0){
+            images.style.transition = "none";
+            images.style.transform = "translateX(-${(total - 1)* width}px)";
+            index = total - 1;
+            setTimeout(() =>{
+                prevImage();
+            },20)
+        }else{showImage(index - 1)}
+    }
+    // setInterval(() => { nextImage(); },500);
